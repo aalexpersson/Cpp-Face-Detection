aa264b336a58c00c67f7443eb59fcfda975117b3
aalexpersson <alex.persson0408@gmail.com>
2023-06-18 21:19:04 +0200

    [Add] Captures and displays live webcam footage.

--- Cpp-Face-Detection/main.cpp
+++ Cpp-Face-Detection/main.cpp
@@ -1,6 +1,45 @@
+﻿#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
 #include <iostream>
 
+using namespace cv;
+using namespace std;
+
 int main() {
-	std::cout << "Hello World" << std::endl;
-	return 0;
+    VideoCapture cam(0);
+
+    // Check if camera opened successfully
+    if (!cam.isOpened()) {
+        cout << "Error opening video stream or file" << endl;
+        return -1;
+    }
+
+    while (1) {
+
+        Mat frame;
+        // Capture frame-by-frame
+        cam >> frame;
+
+        // If the frame is empty, break immediately
+        if (frame.empty())
+            break;
+
+        // Display the video capture
+        imshow("Video capture", frame);
+
+        // Press  ESC on keyboard to exit
+        char c = (char)waitKey(25);
+        if (c == 27)
+            break;
+    }
+
+    // When everything done, release the video capture object
+    cam.release();
+
+    // Closes all the frames
+    destroyAllWindows();
+
+    return 0;
 }

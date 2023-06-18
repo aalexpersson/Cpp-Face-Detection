b8579d65b369fa5b202ccea7c36c6106e14366f9
aalexpersson <alex.persson0408@gmail.com>
2023-06-18 23:10:03 +0200

    [Add] Detects and draws a rectangle around at least one face, haven't tested multiple.

--- Cpp-Face-Detection/main.cpp
+++ Cpp-Face-Detection/main.cpp
@@ -1,4 +1,4 @@
-﻿#include "opencv2/objdetect.hpp"
+﻿#include <opencv2/objdetect/objdetect.hpp>
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
@@ -16,7 +16,12 @@ int main() {
         return -1;
     }
 
-    while (1) {
+    string trained_classifier_location = "C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml";//Defining the location our XML Trained Classifier in a string
+    CascadeClassifier faceDetector;//Declaring an object named 'face detector' of CascadeClassifier class//
+    faceDetector.load(trained_classifier_location);//loading the XML trained classifier in the object//
+    vector<Rect>faces;
+
+    while (true) {
 
         Mat frame;
         // Capture frame-by-frame
@@ -26,6 +31,17 @@ int main() {
         if (frame.empty())
             break;
 
+        faceDetector.detectMultiScale(frame, faces, 1.1, 4, CASCADE_SCALE_IMAGE, Size(70, 70));//Detecting the faces in 'image_with_humanfaces' matrix//
+        cam.read(frame);// reading frames from camera and loading them in 'frame' Matrix//
+        for (int i = 0; i < faces.size(); i++) { //for locating the face
+            Mat faceROI = frame(faces[i]);//Storing face in the matrix//
+            int x = faces[i].x;//Getting the initial row value of face rectangle's starting point//
+            int y = faces[i].y;//Getting the initial column value of face rectangle's starting point//
+            int h = y + faces[i].height;//Calculating the height of the rectangle//
+            int w = x + faces[i].width;//Calculating the width of the rectangle//
+            rectangle(frame, Point(x, y), Point(w, h), Scalar(255, 0, 255), 2, 8, 0);//Drawing a rectangle using around the faces//
+        }
+
         // Display the video capture
         imshow("Video capture", frame);
 
@@ -34,6 +50,7 @@ int main() {
         if (c == 27)
             break;
     }
+
 
     // When everything done, release the video capture object
     cam.release();
